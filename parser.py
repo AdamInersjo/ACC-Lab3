54eb250979b99ea9b2b3a4696ea7d0249179fbdd
AdamInersjo <adam.inersjo.2778@student.uu.se>
2019-10-02 12:21:55 +0200

    Add documentation to parser functions

--- parser.py
+++ parser.py
@@ -3,8 +3,10 @@ import string
 
 
 def add_to_first(first: dict, second: dict):
+    """Add the values from the second dict to the first, only adding values of keys that are in the first."""
     for key in first:
-        first[key] += second[key]    
+        first[key] += second[key]
+
 
 def parse_test_data():
     with jsonlines.open('test_data.ndjson') as reader:
@@ -13,18 +15,22 @@ def parse_test_data():
             r = find_pronouns(tweet['text'])
             add_to_first(total, r)
     print(total)
-            
+
 
 def find_pronouns(text: str) -> dict:
+    """Count the number of times pronouns are found in a string.
+
+    The pronouns counted are: 'han', 'hon', 'den', 'denna', 'denne' & 'hen' 
+    """
     result = {'han': 0, 'hon': 0, 'den': 0, 'denna': 0, 'denne': 0, 'hen': 0}
     text = text.lower()
     for key in result:
-        result[key]+= count_word_occurances(text, key)
+        result[key] = count_word_occurances(text, key)
     return result
 
 
-# Adaptation from aaronasterling's answer https://stackoverflow.com/a/4155029
 def count_word_occurances(text: str, word: str) -> int:
+    """Return the nuber of times a complete word is found in a string."""
     next_index = 0
     count = 0
     while (next_index < len(text)):
@@ -37,7 +43,13 @@ def count_word_occurances(text: str, word: str) -> int:
     return count
 
 
-def is_word(text, start, end):
+def is_word(text: str, start: int, end: int) -> bool:
+    """Check if a substring with a start and end point is a word.
+
+    A word is defined as a substring that is surrounded by word delimiters.
+    The begining and end of a string constitutes as word delimiters, 
+    as are whitespace and punctuation.
+    """
     if start != 0 and not word_delimiter(text[start-1]):
         return False
     if end <= len(text) - 1 and not word_delimiter(text[end]):
@@ -46,6 +58,7 @@ def is_word(text, start, end):
 
 
 def word_delimiter(letter: str) -> bool:
+    """Check if single-letter string is whitespace or punctuation."""
     return letter in (string.whitespace + string.punctuation)
 
 
